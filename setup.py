ec34d629ebd665f6faa23367abfbe29607a29224
Rounak Sen <rony000013@gmail.com>
2023-02-05 14:22:06 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(name='youtube_downloader',
       version='2.0',
-      description='It is a video downloader for YouTube created using pytube and some other libraries for downloading videos in mp4 format.',
-      url='https://github.com/rony0000013/Youtube-Video-Downloader',
+      description='It is a video downloader for YouTube created using pytube, moviepy and some other libraries for downloading videos in mp4 format.',
+      url='C:\Python Projects\youtube_downloader_full',
       author='rony0000013',
       license='MIT',
       packages=find_packages(),
